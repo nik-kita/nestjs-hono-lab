306af07d19716068122120dd9121c349993360f4
nik-kita <nik@kita.github>
2025-03-24 19:28:57 +0100

    some updates

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,7 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 const app = await NestFactory.create(AppModule);
 const config = new DocumentBuilder().setTitle("NestJS+Deno Lab").build();
 const document = SwaggerModule.createDocument(app, config);
+
 SwaggerModule.setup("api", app, document);
+
 await app.listen(4000);
