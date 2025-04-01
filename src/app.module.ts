831f09217a053596730fd927094241f3b064329f
nik-kita <nik@kita.github>
2025-04-01 18:11:39 +0200

    rm from application prev modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,6 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { resolver_Blog } from "./feature/blog/Resolver_Blog.ts";
-import {
-  DataProvider_Post,
-  Resolver_Post,
-} from "./feature/blog/resolvers/Resolver_Post.ts";
-import { Module_TaskManagement } from "./feature/task-management/Module_TaskManagement.module.ts";
 
 export const playground_plugin = ApolloServerPluginLandingPageLocalDefault() as // deno-lint-ignore no-explicit-any
 any;
@@ -22,12 +17,9 @@ any;
         playground_plugin,
       ],
     }),
-    Module_TaskManagement,
   ],
   providers: [
     resolver_Blog,
-    DataProvider_Post,
-    Resolver_Post,
   ],
 })
 export class AppModule {}
